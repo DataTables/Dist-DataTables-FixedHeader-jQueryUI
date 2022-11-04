b4b4e24ee67b25f1e0fc9a369cee7bf0152f7259
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 17:10:09 +0000

    859c87197a3661ae36209bb1cadf6513a9d0d0df
    Sync to source repo @859c87197a3661ae36209bb1cadf6513a9d0d0df

--- js/fixedHeader.jqueryui.min.mjs
+++ js/fixedHeader.jqueryui.min.mjs
@@ -1,1 +1,4 @@
+/*! jQuery UI styling wrapper for FixedHeader
+ * © SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-ju";import DataTable from"datatables.net-fixedheader";export default DataTable;
